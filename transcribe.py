1c0d57fa0ff218087b42299a54bc7cb3b8ba07e9
Yasuhiro Morioka <ymorioka@gmail.com>
2025-02-15 17:05:38 +0900

    use tempfile

--- transcribe.py
+++ transcribe.py
@@ -4,15 +4,7 @@ from moviepy import VideoFileClip
 
 import argparse
 import os
-
-def extract_audio_from_video(video_path):
-    try:
-        clip = VideoFileClip(video_path)
-        audio_path = video_path.rsplit(".", 1)[0] + ".wav"
-        clip.audio.write_audiofile(audio_path, codec="pcm_s16le")
-        return audio_path
-    except Exception as e:
-        return None
+import tempfile
 
 def main(args):
     sample = args.audio_file
@@ -23,39 +15,40 @@ def main(args):
     if not os.path.isfile(sample):
         return
 
-    # 動画ファイルなら音声部分を抽出する
-    temp_audio_path = None
-    ext = sample.lower().rsplit(".", 1)[-1]
-    if ext in ["mp4", "mkv", "avi", "mov"]:
-        temp_audio_path = extract_audio_from_video(sample)
-        if not temp_audio_path:
-            return  # 失敗時は処理を中断
-        sample = temp_audio_path  # 書き起こし対象を音声ファイルに変更
+    with tempfile.NamedTemporaryFile(suffix='wav', delete=False) as fp:
+        fp.close()
+    
+        # 動画ファイルなら音声部分を抽出する
+        ext = sample.lower().rsplit(".", 1)[-1]
+        if ext in ["mp4", "mkv", "avi", "mov"]:
+            clip = VideoFileClip(sample)
+            clip.audio.write_audiofile(fp.name, codec="pcm_s16le")
+            sample = fp.name    # 書き起こし対象を音声ファイルに変更
 
-    # 以下、ベースは kotoba-whisper-v2.1 サンプルコード
-    # https://huggingface.co/kotoba-tech/kotoba-whisper-v2.1
+        # 以下、ベースは kotoba-whisper-v2.1 サンプルコード
+        # https://huggingface.co/kotoba-tech/kotoba-whisper-v2.1
 
-    # config
-    model_id = "kotoba-tech/kotoba-whisper-v2.1"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    model_kwargs = {"attn_implementation": "sdpa"} if torch.cuda.is_available() else {}
-    generate_kwargs = {"language": language, "task": "transcribe"}
+        # config
+        model_id = "kotoba-tech/kotoba-whisper-v2.1"
+        torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        model_kwargs = {"attn_implementation": "sdpa"} if torch.cuda.is_available() else {}
+        generate_kwargs = {"language": language, "task": "transcribe"}
 
-    # load model
-    pipe = pipeline(
-        model=model_id,
-        torch_dtype=torch_dtype,
-        device=device,
-        model_kwargs=model_kwargs,
-        batch_size=16,
-        trust_remote_code=True,
-        punctuator=punctuation
-    )
+        # load model
+        pipe = pipeline(
+            model=model_id,
+            torch_dtype=torch_dtype,
+            device=device,
+            model_kwargs=model_kwargs,
+            batch_size=16,
+            trust_remote_code=True,
+            punctuator=punctuation
+        )
 
-    # run inference
-    result = pipe(sample, return_timestamps=return_timestamps, generate_kwargs=generate_kwargs)
-    print(result)
+        # run inference
+        result = pipe(sample, return_timestamps=return_timestamps, generate_kwargs=generate_kwargs)
+        print(result)
 
 
 if __name__ == '__main__':
